81378b3c26977c38242ce5a567c265a9d5431949
island205 <island205@gmail.com>
2014-02-09 20:51:58 +0800

    split app from tutorial

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	//   mongodb: 'mongodb://localhost/technodechapter02',
+  mongodb: 'mongodb://technode:technode@troup.mongohq.com:10046/technode',
+  robot: {
+    name: 'technode机器人',
+    avatarUrl: '/images/technode.png'
+  }
+}
